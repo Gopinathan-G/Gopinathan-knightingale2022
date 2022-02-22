1757ad32174103719bd35f2f1167fddf5fb845e6
Kesavan.G <Kesavan.G@Kesavan>
2022-02-21 19:50:37 -0800

    My first Commit

---
+++ gopi/src/gopi/Programmer.java
@@ -0,0 +1,23 @@
+package gopi;
+
+
+public class Programmer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=25;
+	
+    	method(n);
+
+
+    }
+    public static void method (int x) {
+    	x++;
+    	int z = x;
+    	z = z - 4;
+   
+   
+
+	}
+
+}
